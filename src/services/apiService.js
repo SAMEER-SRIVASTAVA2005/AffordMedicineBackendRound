cd3cab8727bac6729c8d26c7da457c2ca95d3213
Sameer Srivastava <sameersrivastav83033@gmail.com>
2026-05-14 17:00:17 +0530

    second commit

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,20 +1,10 @@
 const axios = require("axios");
 
 async function fetchVehicles(token) {
-    const response = await axios.get(
-        `${BASE_URL}/vehicles`,
-        {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }
-    );
-
-    return response.data;
+    
 }
 
 
 module.exports = {
-    fetchVehicles,
-    fetchDepots
+    fetchVehicles
 };
--- src/utils/knapsack.js
+++ src/utils/knapsack.js
@@ -1,12 +1,6 @@
 function knapsack(tasks, maxHours) {
 
-    const n = tasks.length;
-
-    const dp = Array.from(
-        { length: n + 1 },
-        () => Array(maxHours + 1).fill(0)
-    );
-
+    
     
 }
 
